918655c1dea0d4696f3d6e8cdbe149f90413aa79
marco76tv <marco.sottana@gmail.com>
2023-12-20 15:49:12 +0100

    first

---
+++ Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Seo\Providers\Filament;
+
+use Modules\Xot\Providers\Filament\XotBasePanelProvider;
+
+class AdminPanelProvider extends XotBasePanelProvider
+{
+    protected string $module = 'Seo';
+}
