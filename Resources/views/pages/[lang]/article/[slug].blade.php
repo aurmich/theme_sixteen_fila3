7cfdfd50916356d94fbbeb7c1c590181306ba127
marco76tv <marco.sottana@gmail.com>
2024-04-25 22:41:11 +0200

    up

--- Resources/views/pages/[lang]/article/[slug].blade.php
+++ Resources/views/pages/[lang]/article/[slug].blade.php
@@ -21,7 +21,7 @@ render(function (View $view, string $slug) {
     foreach($missing_slugs as $row){
         $data=[
             'row'=>$row,
-            'title'=>$row->title,
+            'title'=>$row->attributes['title'],
             'slug'=>Str::slug($row->title)
             ];
         dd($data);
