02669ec5be60d87c26727299bdaecb5d0bc3a1ef
Malebestia <vair81@gmail.com>
2024-07-29 12:45:06 +0200

    .

--- Resources/views/components/ui/marketing/header.blade.php
+++ Resources/views/components/ui/marketing/header.blade.php
@@ -16,8 +16,8 @@
             </div>
             
             <div :class="{ 'flex' : mobileMenuOpen, 'hidden md:flex' :  !mobileMenuOpen }" class="fixed top-0 left-0 z-40 flex-col items-start justify-start hidden w-full h-full min-h-screen pt-20 space-y-5 text-sm font-medium duration-150 ease-out transform md:pt-0 text-neutral-500 md:h-auto md:min-h-0 md:left-auto md:items-center md:relative">
-
                 <nav class="flex flex-col w-full p-6 space-y-2 bg-white md:p-0 md:flex-row md:space-x-2 md:space-y-0 md:w-auto md:bg-transparent md:flex">
+                    <x-ui.nav-link href="/">Home</x-ui.nav-link>
                     @foreach($_theme->getMenu('headernav_right') as $item)
                         <x-ui.nav-link href="{{ $_theme->getMenuUrl($item) }}">{{ $item['title'] }}</x-ui.nav-link>
                     @endforeach
@@ -35,22 +35,13 @@
            
         </div>
         <div class="relative z-50 flex items-stretch space-x-3 text-neutral-800">
-            {{-- @foreach($_theme->getMenu('headernav_right') as $item)
-            <div class="flex items-center w-auto">
-
-                <x-ui.button type="success" submit="true" tag="a" href="{{ $_theme->getMenuUrl($item) }}" wire:click='ciao'>{{ $item['title'] }}</x-ui.button>
-
-
-            </div>
-            @endforeach --}}
-
             <x-ui.language ></x-ui.language>
             <div x-data class="flex-shrink-0 hidden w-[38px] overflow-hidden rounded-full h-[38px] sm:block" x-cloak>
                 <x-ui.light-dark-switch></x-ui.light-dark-switch>
             </div>
             @auth
                 <div class="flex items-center w-auto">
-                    <x-ui.button type="primary" submit="true" tag="a" href="{{ route('dashboard') }}">View Dashboard</x-ui.button>
+                    <x-ui.button type="primary" submit="true" tag="a" href="{{ route('profile.edit') }}">View Dashboard</x-ui.button>
                 </div>
             @else
                 <div class="flex items-center w-auto">
