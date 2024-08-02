a5f51c5489dc7384da03a2ec791f70169e15557c
marco <marco.sottana@gmail.com>
2024-08-02 11:41:03 +0200

    🔧 (AdminPanelProvider.php): remove commented out code and unnecessary conflict markers to clean up the file and improve readability

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -13,21 +13,12 @@ class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Fixcity';
 
-<<<<<<< HEAD
     public function panel(Panel $panel): Panel
     {
         $panel->plugins([
             // FilamentPeekPlugin::make(),
             // SpatieLaravelTranslatablePlugin::make(),
         ]);
-=======
-    // public function panel(Panel $panel): Panel
-    // {
-    //     $panel->plugins([
-    //         // FilamentPeekPlugin::make(),
-    //         //SpatieLaravelTranslatablePlugin::make(),
-    //     ]);
->>>>>>> adec8b0 (.)
 
     //     return parent::panel($panel);
     // }
