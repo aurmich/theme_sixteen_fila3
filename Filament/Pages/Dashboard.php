7e220135af0f1afa73cced7e11349fd9aa437fc4
marco <marco.sottana@gmail.com>
2024-08-02 11:39:56 +0200

    🔧 (Dashboard.php): Remove unnecessary code and resolve conflicts in Dashboard.php file 🔧 (AdminPanelProvider.php): Remove unnecessary code and resolve conflicts in AdminPanelProvider.php file 🔧 (module.json): Remove unnecessary code and resolve conflicts in module.json file

--- Filament/Pages/Dashboard.php
+++ Filament/Pages/Dashboard.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Fixcity\Filament\Pages;
 
-<<<<<<< HEAD
 // use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Page;
 
@@ -13,13 +12,4 @@ class Dashboard extends Page
     protected static ?string $navigationIcon = 'heroicon-o-home';
 
     protected static string $view = 'fixcity::filament.pages.dashboard';
-=======
-use Filament\Pages\Dashboard as BaseDashboard;
-
-class Dashboard extends BaseDashboard
-{
-    protected static ?string $navigationIcon = 'heroicon-o-home';
-
-    // protected static string $view = 'gdpr::filament.pages.dashboard';
->>>>>>> b14a224 (up)
 }
