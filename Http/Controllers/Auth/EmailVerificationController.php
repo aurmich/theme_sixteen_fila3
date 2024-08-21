92bcfd77da883c25bdde7b0191816db216431372
marco <marco.sottana@gmail.com>
2024-08-21 18:03:45 +0200

    up

--- Http/Controllers/Auth/EmailVerificationController.php
+++ Http/Controllers/Auth/EmailVerificationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Themes\Sixteen\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Access\AuthorizationException;
