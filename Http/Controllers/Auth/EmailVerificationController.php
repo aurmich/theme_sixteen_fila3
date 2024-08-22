ec613f7840a1ce18bc05d8b5b8fc5365206ffc9f
marco <marco.sottana@gmail.com>
2024-08-22 11:56:39 +0200

    up

--- Http/Controllers/Auth/EmailVerificationController.php
+++ Http/Controllers/Auth/EmailVerificationController.php
@@ -2,7 +2,7 @@
 
 namespace Themes\Sixteen\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Themes\Sixteen\Http\Controllers\Controller;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\RedirectResponse;
--- Http/Controllers/Auth/LogoutController.php
+++ Http/Controllers/Auth/LogoutController.php
@@ -2,7 +2,7 @@
 
 namespace Themes\Sixteen\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Themes\Sixteen\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
