603f227f688161cdb2409e776a3a6f34dae9d0fc
marco76tv <marco.sottana@gmail.com>
2024-08-28 11:56:32 +0200

    up

--- Resources/views/pages/index.blade.php
+++ Resources/views/pages/index.blade.php
@@ -2,19 +2,43 @@
 
 use function Laravel\Folio\{middleware, name};
 use Filament\Notifications\Notification;
+use Filament\Notifications\Livewire\Notifications;
+use Filament\Notifications\Actions\Action;
+use Filament\Support\Enums\Alignment;
+use Filament\Support\Enums\VerticalAlignment;
 use Livewire\Volt\Component;
 
 name('home');
 //middleware(['redirect-to-dashboard']);
+
+//dddx(auth()->user());
 
 new class extends Component
 {
     public function test()
     {
 
+        //Notifications::alignment(Alignment::Center);
+        //Notifications::verticalAlignment(VerticalAlignment::Center);
         Notification::make()
+            //->alignment(Alignment::Center)
+            //->verticalAlignment(VerticalAlignment::Center)
+            ->icon('heroicon-o-document-text')
+            ->iconColor('success')
             ->title('Test')
-            ->success()
+            ->body('Changes to the post have been saved.')
+            //->persistent()
+            ->duration(5000)
+            //->success()
+            ->actions([
+                //Action::make('view')
+               //     ->button(),
+                Action::make('close')
+                    ->button()
+                    ->icon('heroicon-o-document-text')
+                    ->color('gray')
+                    ->close(),
+            ])
             ->send();
     }
 
@@ -23,7 +47,12 @@ new class extends Component
 ?>
 
 <x-layouts.marketing>
-    <button wire:click="notify()">notify</button>
-    {!! $_theme->showPageContent('home') !!}
-
+    @volt
+    <div>
+        {{--
+        <span wire:init="test()"></span>
+        --}}
+        {!! $_theme->showPageContent('home') !!}
+    </div>
+    @endvolt
 </x-layouts.marketing>
