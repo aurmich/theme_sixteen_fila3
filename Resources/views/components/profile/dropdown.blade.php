07204eb4edaa048e98a11b84850ba4f26d3c842b
marco <marco.sottana@gmail.com>
2024-09-27 16:09:36 +0200

    up

--- Resources/views/components/profile/dropdown.blade.php
+++ Resources/views/components/profile/dropdown.blade.php
@@ -7,10 +7,11 @@
     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700"
         id="profile-dropdown-menu">
         <ul class="py-2 font-medium" role="none">
-            @if($_profile->user->hasRole('super-admin'))
+            
+            @can('go-to-admin')
             <li>
 
-              <a href="{{ url('idoteca/admin/profiles') }}"
+              <a href="{{ url('idoteca/admin/') }}"
                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
                   role="menuitem">
                   <div class="inline-flex items-center">
@@ -21,7 +22,7 @@
               </a>
 
             </li>
-            @endif
+            @endcan
             <li>
 
                 <a href="{{ route('logout') }}"
