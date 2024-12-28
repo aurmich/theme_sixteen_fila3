23a06480934e3713ec3a7951907db1b2e6a69f7b
marco76tv <marco.sottana@gmail.com>
2024-12-28 22:18:32 +0100

    up

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
+
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
@@ -21,6 +22,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
         @filamentStyles
+
         @vite(['resources/css/app.css'],'themes/Sixteen/dist')
 
         <!-- CSRF Token -->
