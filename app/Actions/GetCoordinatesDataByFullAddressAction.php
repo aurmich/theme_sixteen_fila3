30e633630b3587279d4d9171295c93f7905388a5
marco76tv <marco.sottana@gmail.com>
2025-01-14 14:19:02 +0100

    up

---
+++ app/Actions/GetCoordinatesDataByFullAddressAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Geo\Actions;
+
+class GetCoordinatesDataByFullAddressAction
+{
+    public function execute(string $address): array
+    {
+        // Simulate fetching coordinates for the given address
+        // In real implementation, this could be an API call to a geocoding service
+        // Here we return dummy coordinates for demonstration purposes
+        return [
+            'latitude' => 40.7128,  // Example latitude
+            'longitude' => -74.0060 // Example longitude
+        ];
+    }
+}
