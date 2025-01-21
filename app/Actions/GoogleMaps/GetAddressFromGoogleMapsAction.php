40177ff854b2bc17f313283e344b61f3ab149ea8
marco <marco.sottana@gmail.com>
2025-01-21 22:26:01 +0100

    up

--- app/Actions/GoogleMaps/GetAddressFromGoogleMapsAction.php
+++ app/Actions/GoogleMaps/GetAddressFromGoogleMapsAction.php
@@ -43,6 +43,10 @@ class GetAddressFromGoogleMapsAction
 
         $data = $response->json();
 
+        dddx([
+            'data' => $data,
+        ]);
+
         if (empty($data['results'][0])) {
             return null;
         }
