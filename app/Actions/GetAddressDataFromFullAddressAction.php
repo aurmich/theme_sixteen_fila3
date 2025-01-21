df7f33845a4854d65239164286bd842a9e1ad998
marco <marco.sottana@gmail.com>
2025-01-21 21:17:00 +0100

    up

--- app/Actions/GetAddressDataFromFullAddressAction.php
+++ app/Actions/GetAddressDataFromFullAddressAction.php
@@ -8,11 +8,7 @@ use Modules\Geo\Actions\GoogleMaps\GetGeocodingDataAction;
 use Modules\Geo\Datas\GeocodingData;
 
 /**
-<<<<<<< HEAD
- * Classe per ottenere i dati dell'indirizzo da diversi servizi di geocoding.
-=======
  * Classe per ottenere i dati dell'indirizzo utilizzando diversi servizi di geocoding.
->>>>>>> 023759a (up)
  */
 class GetAddressDataFromFullAddressAction
 {
@@ -29,13 +25,6 @@ class GetAddressDataFromFullAddressAction
     }
 
     /**
-<<<<<<< HEAD
-     * Esegue la ricerca dell'indirizzo su tutti i servizi disponibili.
-     *
-     * @param string $address L'indirizzo da cercare
-     *
-     * @return AddressData|null I dati dell'indirizzo trovato o null se non trovato
-=======
      * Ottiene i dati dell'indirizzo da un indirizzo completo.
      *
      * @param string $fullAddress L'indirizzo da cercare
@@ -43,7 +32,6 @@ class GetAddressDataFromFullAddressAction
      * @throws \RuntimeException Se la richiesta fallisce o l'indirizzo non viene trovato
      *
      * @return GeocodingData I dati dell'indirizzo trovato
->>>>>>> 023759a (up)
      */
     public function execute(string $fullAddress): GeocodingData
     {
@@ -54,17 +42,6 @@ class GetAddressDataFromFullAddressAction
         } catch (\Exception $e) {
             throw new \RuntimeException('Errore durante il recupero dei dati dell\'indirizzo: '.$e->getMessage());
         }
-<<<<<<< HEAD
-
-        if ($this->errors->isNotEmpty()) {
-            Notification::make()
-                ->warning()
-                ->title('Geocoding Error')
-                ->body($this->errors->join("\n"))
-                ->send();
-        }
-
-        return null;
     }
 
     /**
@@ -75,7 +52,5 @@ class GetAddressDataFromFullAddressAction
     public function getErrors(): Collection
     {
         return $this->errors;
-=======
->>>>>>> 023759a (up)
     }
 }
