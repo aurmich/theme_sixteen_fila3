953100b2de56117652467deef985c1a5d10ade14
marco76tv <marco76tv@users.noreply.github.com>
2025-01-27 09:03:45 +0000

    Check & fix styling

--- Exceptions/InvalidElevationDataException.php
+++ Exceptions/InvalidElevationDataException.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Geo\Exceptions;
 
-use Exception;
-
-class InvalidElevationDataException extends Exception
+class InvalidElevationDataException extends \Exception
 {
-    public function __construct(string $message = 'Invalid elevation data', int $code = 0, ?Exception $previous = null)
+    public function __construct(string $message = 'Invalid elevation data', int $code = 0, ?\Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
