c8370a72107dcc77625d4a4decc97044ded2df6c
marco76tv <marco.sottana@gmail.com>
2025-02-03 10:02:49 +0100

    up

--- app/Datas/BingMapData.php
+++ app/Datas/BingMapData.php
@@ -4,23 +4,53 @@ declare(strict_types=1);
 
 namespace Modules\Geo\Datas;
 
+use Spatie\LaravelData\Data;
+
 /**
- * Data Transfer Object per i dati delle mappe Bing.
+ * Classe per gestire i dati grezzi di Bing Maps.
  */
-class BingMapData
+class BingMapData extends Data
 {
     /**
-     * @param array<string, mixed> $data Dati grezzi dalla risposta di Bing Maps
+     * @param array{
+     *     point: array{
+     *         coordinates: array{0: float, 1: float}
+     *     },
+     *     address: array{
+     *         countryRegion: ?string,
+     *         adminDistrict: ?string,
+     *         adminDistrict2: ?string,
+     *         locality: ?string,
+     *         postalCode: ?string,
+     *         addressLine: ?string,
+     *         countryRegionIso2: ?string,
+     *         neighborhood: ?string
+     *     }
+     * } $data
      */
     public function __construct(
-        private readonly array $data,
+        private readonly array $data
     ) {
     }
 
     /**
-     * Converte l'oggetto in un array.
+     * Converte i dati in un array.
      *
-     * @return array<string, mixed>
+     * @return array{
+     *     point: array{
+     *         coordinates: array{0: float, 1: float}
+     *     },
+     *     address: array{
+     *         countryRegion: ?string,
+     *         adminDistrict: ?string,
+     *         adminDistrict2: ?string,
+     *         locality: ?string,
+     *         postalCode: ?string,
+     *         addressLine: ?string,
+     *         countryRegionIso2: ?string,
+     *         neighborhood: ?string
+     *     }
+     * }
      */
     public function toArray(): array
     {
