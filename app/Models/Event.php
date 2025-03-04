15732387fe743e3c6845fd25e60fdab30a881ee2
marco76tv <marco.sottana@gmail.com>
2025-03-04 19:18:56 +0100

    up

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,17 +8,17 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Crypt;
-use Modules\Xot\Contracts\ProfileContract;
+use Modules\Xot\Traits\Updater;
 
 use function Safe\json_encode;
 
 /**
  * Modules\Gdpr\Models\Event.
  *
-<<<<<<< HEAD
  * @property string $id
  * @property string|null $treatment_id
  * @property string|null $consent_id
@@ -48,129 +48,37 @@ use function Safe\json_encode;
  * @property string|null $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property string|null $deleted_by
-=======
- * @property string                          $id
- * @property string|null                     $treatment_id
- * @property string|null                     $consent_id
- * @property string                          $subject_id
- * @property string                          $ip
- * @property string                          $action
- * @property string                          $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property Consent|null                    $consent
- *
- * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Event   newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereConsentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   wherePayload($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereUpdatedAt($value)
- *
- * @property string|null                     $updated_by
- * @property string|null                     $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null                     $deleted_by
->>>>>>> origin/dev
  *
  * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedBy($value)
- *
-<<<<<<< HEAD
- * @property string $id
- * @property string|null $treatment_id
- * @property string|null $consent_id
- * @property string $subject_id
- * @property string $ip
- * @property string $action
- * @property string $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $updated_by
- * @property string|null $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $deleted_by
- * @property Consent|null $consent
- *
- * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereConsentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event wherePayload($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedBy($value)
-=======
- * @property string                          $id
- * @property string|null                     $treatment_id
- * @property string|null                     $consent_id
- * @property string                          $subject_id
- * @property string                          $ip
- * @property string                          $action
- * @property string                          $payload
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null                     $updated_by
- * @property string|null                     $created_by
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null                     $deleted_by
- * @property Consent|null                    $consent
- *
- * @method static \Modules\Gdpr\Database\Factories\EventFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Event   newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereConsentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereIp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   wherePayload($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereSubjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereTreatmentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event   whereUpdatedBy($value)
->>>>>>> origin/dev
  *
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
  *
  * @mixin \Eloquent
  */
-class Event extends BaseModel
+class Event extends Model
 {
-    use HasUuids;
+    use HasUuids, Updater;
 
     // protected $table = 'event';
 
-    public $fillable = [
+    protected $fillable = [
         'id',
         'action',
         'treatment_id',
         'consent_id',
         'subject_id',
+        'ip',
         'payload',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 
     public function consent(): BelongsTo
