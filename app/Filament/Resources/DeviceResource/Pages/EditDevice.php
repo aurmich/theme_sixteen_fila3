966ccea7c818c4dbefde79d452bef20b82fac5d9
marco76tv <marco.sottana@gmail.com>
2025-03-06 16:31:46 +0100

    Auto update submodule

--- app/Filament/Resources/DeviceResource/Pages/EditDevice.php
+++ app/Filament/Resources/DeviceResource/Pages/EditDevice.php
@@ -7,14 +7,13 @@ namespace Modules\User\Filament\Resources\DeviceResource\Pages;
 use Filament\Actions\DeleteAction;
 use Modules\User\Filament\Resources\DeviceResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+
+/**
+ * EditDevice
+ */
 class EditDevice extends XotBaseEditRecord
 {
     protected static string $resource = DeviceResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            DeleteAction::make(),
-        ];
-    }
+    
 }
