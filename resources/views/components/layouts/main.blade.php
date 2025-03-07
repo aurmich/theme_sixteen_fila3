a721c49ef342c1a060e336756c4c2fc6a8750207
marco76tv <marco.sottana@gmail.com>
2025-03-07 08:48:57 +0100

    .

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
+
 
         {!! $_theme->metatags() !!}
         <!-- Used to add dark mode right away, adding here prevents any flicker -->
@@ -22,16 +22,16 @@
 		@filamentStyles
 
 
-        @vite([/*'Resources/css/filament/theme.css',*/'Resources/css/app.css',],'themes/Sixteen/dist')
+        @vite([/*'resources/css/filament/theme.css',*/'resources/css/app.css',],'themes/Sixteen/dist')
 
-        
+
     </head>
     <body class="min-h-screen antialiased bg-white dark:bg-gradient-to-b dark:from-gray-950 dark:to-gray-900">
         {{ $slot }}
         <livewire:toast />
         @livewire('notifications')
 		@filamentScripts
-        @vite(['Resources/js/app.js'],'themes/Sixteen/dist')
+        @vite(['resources/js/app.js'],'themes/Sixteen/dist')
         <link rel="stylesheet" type="text/css" href="{{asset("vendor/cookie-consent/css/cookie-consent.css")}}">
     </body>
 </html>
