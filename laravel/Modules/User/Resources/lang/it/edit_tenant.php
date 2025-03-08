d2f165b194b18a0a345e84e1b06269a43575abdc
marco76tv <marco.sottana@gmail.com>
2025-03-08 19:18:23 +0100

    rebase 30

--- laravel/Modules/User/Resources/lang/it/edit_tenant.php
+++
@@ -1,13 +0,0 @@
-<?php return array (
-  'actions' => 
-  array (
-    'view' => 
-    array (
-      'label' => 'view',
-    ),
-    'delete' => 
-    array (
-      'label' => 'delete',
-    ),
-  ),
-);
