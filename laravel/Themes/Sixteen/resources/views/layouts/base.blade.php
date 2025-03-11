3d031479e8073fc71ff76b8d2ce3e5fb8560981e
marco76tv <marco.sottana@gmail.com>
2025-03-11 12:02:42 +0100

    rebase 30

--- laravel/Themes/Sixteen/resources/views/layouts/base.blade.php
+++ laravel/Themes/Sixteen/resources/views/layouts/base.blade.php
@@ -5,22 +5,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 12c05b24a2 (**Remove unnecessary files and directories from the Setting module**)
+=======
+
+>>>>>>> 301f685410 (.)
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
         @endif
+<<<<<<< HEAD
 
         <!-- Favicon -->
 		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
+=======
+        <!-- Favicon -->
+		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+>>>>>>> 301f685410 (.)
         <style>
             [x-cloak] {
                 display: none !important;
             }
         </style>
+<<<<<<< HEAD
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
@@ -30,6 +40,12 @@
 =======
         @vite(['resources/css/app.css'],'themes/Sixteen/dist')
 >>>>>>> 12c05b24a2 (**Remove unnecessary files and directories from the Setting module**)
+=======
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+        @filamentStyles
+        @vite(['resources/css/app.css'],'themes/Sixteen/dist')
+>>>>>>> 301f685410 (.)
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -42,6 +58,7 @@
 
         @filamentScripts
 <<<<<<< HEAD
+<<<<<<< HEAD
         @vite(['resources/js/app.js'],'themes/One/dist')
     </body>
 </html>
@@ -50,3 +67,8 @@
     </body>
 </html>
 >>>>>>> 12c05b24a2 (**Remove unnecessary files and directories from the Setting module**)
+=======
+        @vite(['resources/js/app.js'],'themes/Sixteen/dist')
+    </body>
+</html>
+>>>>>>> 301f685410 (.)
