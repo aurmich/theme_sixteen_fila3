aa3857991671d7bfdc462e3f3b706507d5037b10
marco76tv <marco.sottana@gmail.com>
2025-05-07 16:58:22 +0200

    .

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,19 +9,8 @@ use Filament\Support\Enums\VerticalAlignment;
 use Livewire\Volt\Component;
 use Modules\Tenant\Services\TenantService;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-/** @var array */
-=======
-// @var array
->>>>>>> 8c03ed1 (.)
-//$middleware=TenantService::config('middleware');
-//$base_middleware=Arr::get($middleware,'base',[]);
-$base_middleware=[];
-=======
 // @var array
 $base_middleware = [];
->>>>>>> 721b79a (.)
 
 name('home');
 middleware($base_middleware);
@@ -35,22 +24,7 @@ new class extends Component
 <x-layouts.marketing>
     @volt('home')
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c03ed1 (.)
-        {{--
-        {!! $_theme->showPageContent('home') !!}
-        --}}
         <x-page-content slug="home" />
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c03ed1 (.)
-=======
-        <x-page-content slug="home" />
->>>>>>> 721b79a (.)
     </div>
     @endvolt
 </x-layouts.marketing>
