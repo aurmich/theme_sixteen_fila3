84fe7ff7d2122e0b81064bde8dde6119fb522244
marco76tv <marco.sottana@gmail.com>
2025-05-15 13:01:37 +0200

    .

--- Resources/css/filament/tailwind.config.js
+++ Resources/css/filament/tailwind.config.js
@@ -7,18 +7,8 @@ export default {
         '../../resources/views/filament/**/*.blade.php',
         //'../../vendor/filament/**/*.blade.php',
         '../../vendor/filament/**/*.blade.php',
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        '../../Modules/**/resources/views/**/*.blade.php',
-        '../../Themes/**/resources/views/**/*.blade.php',
-=======
-=======
-        '../../Modules/**/resources/views/**/*.blade.php',
-        '../../Themes/**/resources/views/**/*.blade.php',
->>>>>>> Stashed changes
         '../../Modules/**/Resources/views/**/*.blade.php',
         '../../Themes/**/Resources/views/**/*.blade.php',
->>>>>>> 90e5aa2 (.)
         '../../Modules/**/Filament/**/*.php',
         '../../../public_html/vendor/**/*.blade.php',
     ],
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,20 +9,11 @@ use Filament\Support\Enums\VerticalAlignment;
 use Livewire\Volt\Component;
 use Modules\Tenant\Services\TenantService;
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 /** @var array $base_middleware */
 // Per configurazioni avanzate:
 // $middleware = TenantService::config('middleware');
 // $base_middleware = Arr::get($middleware, 'base', []);
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
 // @var array
->>>>>>> 90e5aa2 (.)
 $base_middleware = [];
 
 name('home');
@@ -37,18 +28,9 @@ new class extends Component
 <x-layouts.marketing>
     @volt('home')
     <div>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
         {{-- Per mostrare il contenuto custom del tema:
         {!! $_theme->showPageContent('home') !!}
         --}}
-=======
->>>>>>> 90e5aa2 (.)
-=======
-        {{-- Per mostrare il contenuto custom del tema:
-        {!! $_theme->showPageContent('home') !!}
-        --}}
->>>>>>> Stashed changes
         <x-page-content slug="home" />
     </div>
     @endvolt
