b81203390041794b0e0d6059db4d737ec9827dc6
marco76tv <marco.sottana@gmail.com>
2025-08-29 18:55:14 +0200

    .

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,13 @@
 <x-layouts.main>
+<<<<<<< HEAD
     <x-section slug="header"/>
     {{ $slot }}
     <x-section slug="footer"/>
+=======
+<x-section slug="header"/>
+    {{ $slot }}
+<x-section slug="footer"/>
+    {{ $_theme->footer() }}
+
+>>>>>>> 029971d (.)
 </x-layouts.main>
--- resources/views/components/sections/header.blade.php
+++ resources/views/components/sections/header.blade.php
@@ -92,6 +92,7 @@
         <div class="flex justify-between w-full max-w-screen-xl mx-auto">
             <div class="flex-1">
                 <a href="" class="flex items-center py-2 space-x-4">
+<<<<<<< HEAD
                     {{--  
                     <x-heroicon-o-shield-check class="stroke-1 size-16" />
                     --}}
@@ -100,6 +101,12 @@
                     <div class="text-start">
                         <div class="text-2xl font-bold">{{ $_theme->metatag('title') }}</div>
                         <div class="text-sm">{{ $_theme->metatag('subtitle') }}</div>
+=======
+                    <x-heroicon-o-shield-check class="stroke-1 size-16" />
+                    <div class="text-start">
+                        <div class="text-2xl font-bold">Il mio Comune</div>
+                        <div class="text-sm">Un comune da vivere</div>
+>>>>>>> 029971d (.)
                     </div>
                 </a>
             </div>
@@ -128,7 +135,10 @@
     <div class="hidden h-12 overflow-auto min-h-12 navbar md:flex">
         <div class="flex justify-between w-full max-w-screen-xl mx-auto space-x-12">
             <div class="flex-1 ">
+<<<<<<< HEAD
                 {{--  dddx(get_defined_vars()) --}}
+=======
+>>>>>>> 029971d (.)
                 <ul class="items-center px-1 menu menu-horizontal flex-nowrap">
                     <li><a href="">Amministrazione</a></li>
                     <li><a href="">Novità</a></li>
