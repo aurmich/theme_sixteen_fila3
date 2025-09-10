b35ac0468fdab7cb84716f9be72fe83f8174f5cf
marco76tv <marco.sottana@gmail.com>
2025-09-10 11:02:18 +0200

    .

--- app/Services/ThemeService.php
+++ app/Services/ThemeService.php
@@ -61,7 +61,6 @@ class ThemeService
         return [
             'name' => $this->themeName,
             'version' => $this->version,
-<<<<<<< HEAD
             'author' => 'Laraxot Team',
             'description' => 'Modern responsive theme for Laraxot applications',
             'features' => [
@@ -70,10 +69,6 @@ class ThemeService
                 'component_library',
                 'accessibility_compliant',
             ],
-=======
-            'description' => 'Tema Sixteen ',
-            'author' => 'Team',
->>>>>>> 20018e3 (.)
         ];
     }
 
